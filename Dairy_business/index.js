d37f735f7233ef81a2c130e074472e9bf1aaa70d
DaisyOdongo <daisy.odongo@student.moringaschool.com>
2022-07-17 12:46:33 +0300

    compute earnings acording to the number of days in a month

--- Dairy_business/index.js
+++ Dairy_business/index.js
@@ -6,7 +6,7 @@ function totalProduction(){
 
     var sum = shed_A+shed_B+shed_C+shed_D;
 
-    document.getElementById('total').value ="\nThe total Production in Shed A is "+shed_A +"litres per day"+"\nThe total Production in Shed B is "+shed_B +"litres per day"+"\nThe total Production in Shed C is "+shed_C +"litres per day"+"\nThe total Production in Shed D is "+shed_D +"litres per day"+"\nThe total Production is "+sum +"litres per day"
+    document.getElementById('total').value ="\nThe total Production in Shed A is "+shed_A +"litres per day"+"\nThe total Production in Shed B is "+shed_B +"litres per day"+"\nThe total Production in Shed C is "+shed_C +"litres per day"+"\nThe total Production in Shed D is "+shed_D +"litres per day"+"\nThe total Production is "+sum +"litres per day";
 }
 
 function incomeOverTime(){
@@ -14,9 +14,21 @@ function incomeOverTime(){
     var incomeInWeek = sellingPrice*7 //7 days in a week
     var incomeInYear = sellingPrice * 366 //366 days in a leap year
 
-    document.getElementById("total").value = "\nYour weekly income will be Ksh "+incomeInWeek+"\nYour Yearly income will be Ksh "+incomeInYear
+    document.getElementById("total").value = "\nYour weekly income will be Ksh "+incomeInWeek+"\nYour Yearly income will be Ksh "+incomeInYear;
 }
 
 function monthlyIncome(){
     var sellingPrice =  parseInt(document.getElementById("buyingRate").value);  
+    varincomeJan = sellingPrice * 31;
+    varincomeFeb = sellingPrice * 29;
+    varincomeMarch = sellingPrice * 31;
+    varincomeApril = sellingPrice * 30;
+    varincomeMay = sellingPrice * 31;
+    varincomeJune = sellingPrice * 30;
+    varincomeJuly = sellingPrice * 31;
+    varincomeAugust = sellingPrice * 30;
+    varincomeSep = sellingPrice * 31;
+    varincomeOct = sellingPrice * 30;
+    varincomeNov = sellingPrice * 31;
+    varincomeDec = sellingPrice * 30;
 }
